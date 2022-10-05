4174e0980cd7e8dbe7f75d090bdbcb7fd5d6604d
TabithaKoech <tabbyk1718@gmail.com>
2022-10-05 07:39:36 +0300

    commit tree 17e6960cecce0e086ee38c8f5887f55ed62c50a0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,10 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    student_by_id = Student.find(params[:id])
+    render json: student_by_id
+  end
+
 
 end
